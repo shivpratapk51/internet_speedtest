d5ef1d2a6010db4d1dc094f6adeb055b2c1ecef1
Shivpratp <shivpratap05oct2007@gmail.com>
2024-06-23 19:44:26 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button');
+
+
+
+btn.addEventListener('click', ()=>{
+    let imagelink = 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Tokyo_Sky_Tree_2012.JPG'
+})
